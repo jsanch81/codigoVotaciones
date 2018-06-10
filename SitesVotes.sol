703f02312a7eb698905f267c3d64d99b5374f491
jsanch81 <jsanch81@eafit.edu.co>
2018-06-10 14:40:02 -0500

    mejoras

--- SitesVotes.sol
+++ SitesVotes.sol
@@ -1,26 +1,27 @@
 pragma solidity ^0.4.0;
 contract SistVotes{
-    mapping (int=>bool) registro;
-    mapping (int=>int) voto;
-    mapping (address=>int) escrutinio;
+    mapping (uint=>bool) registro;
+    mapping (uint=>uint) voto;
+    mapping (address=>uint) escrutinio;
+    event Votar(uint indexed CC, address indexed escru);
     
-    function registroVotante(int CC) public {
+    function registroVotante(uint CC) public {
         registro[CC] = true;
         voto[CC] = 1;
     }
     
-    function hacerVoto(int CC, address escru) public {
+    function votar(uint CC, address escru) public {
         if(registro[CC] && voto[CC]==1){
             escrutinio[escru]=1;
             voto[CC]-=1;
         }
     }
     
-    function votantesResgistrados(int CC) public view returns(bool){
+    function votantesResgistrados(uint CC) public view returns(bool){
         return registro[CC];
     }
     
-    function balanceEscrutinio(address escru) public view returns(int totalVotos){
+    function balanceEscrutinio(address escru) public view returns(uint totalVotos){
         return escrutinio[escru];
     } 
     
